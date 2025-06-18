35327832597fcd9491940e0ff3b817045f7e5d64
MicrosexWeb <microsexweb@gmail.com>
2025-06-18 16:28:37 -0400

    Update configUBC.py

--- BACKEND/configUBC.py
+++ BACKEND/configUBC.py
@@ -1,1 +1,18 @@
+# configUBC.py limpio para uso en Flask (sin PyQt5 ni GUI)
+
+# Valores para operar
+A = [0]*8
+B = [0]*8
+var_op = [A, B]
+
+S = [0]*6
+intr = [[0]*8]*4
+R = [0]*8
+C = [0]*9
+
+# Representación de valores en distintos sistemas
+val_h = ["00"]*2
+val_b = ["00000000"]*2
+val_d = ["0"]*2
+val_s = ["0"]*2
 
