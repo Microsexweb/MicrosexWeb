7863ca691a9f5585f3c1a7be79cca2fef12a03e3
Dam89 <77990428+DamarisMartina@users.noreply.github.com>
2025-06-19 15:23:12 -0400

    Same second commit for frontend

---
+++ FRONTEND/script.js
@@ -0,0 +1,11 @@
+function loginUser() {
+  alert("Login simulado.");
+  window.location.href = '/main';
+  return false;
+}
+function goToMenu() {
+  window.location.href = '/main';
+}
+function logout() {
+  window.location.href = '/';
+}
